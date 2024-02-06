318c3c5a0379f5e6607449b99f073ef0fbb872c9
Noah Wilder <noahleviwilder@gmailcom>
2024-02-06 13:48:38 -0500

    Updated model.py for new input size.

--- model.py
+++ model.py
@@ -7,10 +7,14 @@ class eeg_model(nn.Module):
 
     def __init__(self):
         super(eeg_model, self).__init__()
-        num_inputs = 7
+        num_inputs = 25
         num_outputs = 2
         self.layers = nn.Sequential(
-            nn.Linear(num_inputs, 4),
+            nn.Linear(num_inputs, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 4),
             nn.ReLU(),
             nn.Linear(4, num_outputs),
             nn.Sigmoid()
